7c1b5370584f6ce1c8b5033ee0ac1aa4b519b530
niclbody13 <91341366+niclbody13@users.noreply.github.com>
2024-08-30 17:09:04 -0700

    center the spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -6,10 +6,12 @@ import './Spinner.css'
 
 export default function Spinner() {
     return (
-        <div className="spinner">
-            <div className="dot" />
-            <div className="dot" />
-            <div className="dot" />
+        <div className='wrapper'>
+            <div className="spinner">
+                <div className="dot" />
+                <div className="dot" />
+                <div className="dot" />
+            </div>
         </div>
     )
 }
