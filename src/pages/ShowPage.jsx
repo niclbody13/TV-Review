4c16152a859b6d9fd98806231220e75a866746cb
Nicolas Haralambidis <ngh111201@gmail.com>
2024-09-13 18:59:25 -0700

    add new endpoint for season information

--- src/pages/ShowPage.jsx
+++ src/pages/ShowPage.jsx
@@ -17,11 +17,11 @@ const showStyles = css`
     }
 
     .showWrapper h1 {
-        font-size: 2.5rem;
+        font-size: 2.25rem;
         margin-bottom: 0;
     }
 
-    .showWrapper p {
+    .description {
         font-size: 1.35rem;
     }
 
@@ -29,10 +29,36 @@ const showStyles = css`
         font-size: 1.15rem;
         margin-top: 0;
     }
+
+    img {
+      box-shadow: 0 4px 8px 0 black;
+    }
+
+    .mainInfo {
+        display: flex;
+        gap: 4rem;
+        align-items: center;
+    }
+`
+
+const seasonsStyles = css`
+    ul {
+        display: flex;
+        gap: 1rem;
+        overflow-x: scroll;
+        width: 85rem;
+        list-style: none;
+    }
+
+    p {
+        font-size: 1.15rem;
+        margin: 0.5rem 0;
+    }
 `
 
 function ShowPage() {
     const [showData, setShowData] = useState(null)
+    const [seasonsData, setSeasonsData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     let { id } = useParams()
@@ -49,7 +75,7 @@ function ShowPage() {
                     { signal: controller.signal }
                 )
                 if (!response.ok) {
-                    throw new Error('Failed to fetch data');
+                    throw new Error('Failed to fetch data')
                 }
                 const responseBody = await response.json()
                 console.log(responseBody)
@@ -71,18 +97,73 @@ function ShowPage() {
         return () => controller.abort()
     }, [id])
 
+    useEffect(() => {
+        if (!showData) return
+        const controller = new AbortController()
+        async function fetchSeasons() {
+            setLoading(true)
+            try {
+                const response = await fetch(`https://api.tvmaze.com/shows/${id}/seasons`, {
+                    signal: controller.signal,
+                })
+                if (!response.ok) throw new Error('Failed to fetch data')
+                const seasonsResponse = await response.json()
+                setSeasonsData(seasonsResponse)
+                setError(null)
+            } catch (e) {
+                if (e.name === "AbortError") {
+                    console.log("HTTP request was aborted")
+                } else {
+                    console.error("An error has occurred: ", e)
+                    setError(e)
+                }
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchSeasons()
+
+        return () => controller.abort()
+    }, [showData, id])
+
+    function formatDate(date) {
+        const [year, month, day] = date.split('-')
+        return `${month}/${day}/${year}`
+    }
+
+    const seasons = seasonsData.filter(season => season.premiereDate)
+
     return (
         <div css={showStyles}>
             {error && <ErrorContainer>Error: {error.message}</ErrorContainer>}
             {loading && <Spinner />}
             {showData && (
                 <div className='showWrapper'>
-                    <h1>{showData.name}</h1>
-                    <p id='runtime'>({showData.premiered.split('-')[0]} - {showData.ended ? showData.ended.split('-')[0] : ''})</p>
-                    {showData.image && (
-                        <img src={showData.image.medium} alt={`Poster for ${showData.name}`} />
-                    )}
-                    <p dangerouslySetInnerHTML={{ __html: showData.summary }} />
+                    <div className='mainInfo'>
+                        <div>
+                            <h1>{showData.name}</h1>
+                            {seasons.length > 0 && (
+                                <div css={seasonsStyles}>
+                                    <p>{seasons.length} Seasons</p>
+                                    {/* <ul>
+                                    {seasons.map(season => (
+                                        <li key={season.id}>
+                                            <h3>Season {season.number}</h3>
+                                            <p>Premiere: {formatDate(season.premiereDate)}</p>
+                                            <p>End: {formatDate(season.endDate) || '-'}</p>
+                                        </li>
+                                    ))}
+                                </ul> */}
+                                </div>
+                            )}
+                            <p id='runtime'>({showData.premiered.split('-')[0]} - {showData.ended ? showData.ended.split('-')[0] : ''})</p>
+                            {showData.image && (
+                                <img src={showData.image.medium} alt={`Poster for ${showData.name}`} />
+                            )}
+                        </div>
+                    </div>
+                    <p className='description' dangerouslySetInnerHTML={{ __html: showData.summary }} />
                 </div>
             )}
         </div>
