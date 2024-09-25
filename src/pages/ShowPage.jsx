c27baa4bc67cefffdc11aa1fe3d347c6531b33d0
Nicolas Haralambidis <ngh111201@gmail.com>
2024-09-25 16:36:23 -0700

    add basic rating system using the backend api to communicate with the db

--- src/pages/ShowPage.jsx
+++ src/pages/ShowPage.jsx
@@ -1,10 +1,22 @@
 import { css } from '@emotion/react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as filledStar } from '@fortawesome/free-solid-svg-icons'
 
 import noImage from "../assets/no-image.jpg"
 import ErrorContainer from '../components/ErrorContainer'
 import Spinner from '../components/Spinner'
+
+const PORT = import.meta.env.VITE_PORT
+/*
+ * handle deleting by clearing the stars and fetching delete endpoint
+ * handle submit logic with patch and post  
+ * 
+ * 
+
+*/
 
 const showStyles = css`
     display: flex;
@@ -57,11 +69,33 @@ const seasonsStyles = css`
     }
 `
 
+const ratingStyles = css`
+    h1 {
+       margin-bottom: 1rem !important;
+    }
+
+    .starContainer {
+        display: flex;
+        gap: 0.25rem;
+    }
+
+    .star {
+        cursor: pointer;
+    }
+
+    button {
+        margin: 1rem 0;
+        margin-right: 1rem;
+    }
+`
+
 function ShowPage() {
     const [showData, setShowData] = useState(null)
     const [seasonsData, setSeasonsData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
+    const [rating, setRating] = useState(0)
+    const [isRated, setIsRated] = useState(false)
     let { id } = useParams()
 
     useEffect(() => {
@@ -93,6 +127,7 @@ function ShowPage() {
                 setLoading(false)
             }
         }
+
         fetchShows()
 
         return () => controller.abort()
@@ -128,6 +163,26 @@ function ShowPage() {
         return () => controller.abort()
     }, [showData, id])
 
+    useEffect(() => {
+        async function fetchUserRating() {
+            const userId = 1    // set userId to 1 until login is implemented
+            try {
+                const response = await fetch(`http://localhost:${PORT}/ratings/${userId}/${id}`)
+                if (!response.ok) {
+                    const errorData = await response.json()
+                    throw errorData
+                }
+                const ratingData = await response.json()
+                setRating(ratingData.rating || 0)
+                setIsRated(!!ratingData.rating)
+            } catch (e) {
+                console.error('Failed to fetch rating: ', e)
+            }
+        }
+
+        fetchUserRating()
+    }, [id])    //may need to add userId here once there are multiple users
+
     function formatDate(date) {
         const [year, month, day] = date.split('-')
         return `${month}/${day}/${year}`
@@ -135,9 +190,69 @@ function ShowPage() {
 
     const seasons = seasonsData.filter(season => season.premiereDate)
 
+    async function rateShow(showId, rating) {
+        const userId = 1    // set userId to 1 until login is implemented
+        console.log("Rating: ", rating)
+        try {
+            const method = isRated ? 'PATCH' : 'POST'
+            const response = await fetch(`http://localhost:${PORT}/${isRated ? 'updateRating' : 'rateShow'}`, {
+                method,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ userId: userId, showId, rating })
+            })
+            if (!response.ok) {
+                const errorData = await response.json()
+                throw errorData
+            }
+            setError(null)
+            setIsRated(true)
+            alert('Rating saved!')
+        } catch (e) {
+            console.error("An error has occurred: ", e)
+            setError(e.error || 'Failed to rate show')
+        }
+    }
+
+    async function deleteRating(showId) {
+        const userId = 1    // set userId to 1 until login is implemented
+        try {
+            const response = await fetch(`http://localhost:${PORT}/deleteRating/${userId}/${showId}`, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+            })
+            if (!response.ok) {
+                const errorData = await response.json()
+                throw errorData
+            }
+            setError(null)
+            setRating(0)
+            setIsRated(false)
+            alert('Rating deleted!')
+        } catch (e) {
+            if (e.name === "AbortError") {
+                console.log("HTTP request was aborted")
+            } else {
+                console.error("An error has occurred: ", e)
+                setError(e.error || 'Failed to delete rating')
+            }
+        }
+    }
+
+    const toggleStar = (index) => {
+        console.log("star clicked: ", index)
+        console.log("Port: ", PORT)
+        if (index === 0 && rating === 1) {
+            setRating(0)
+            return
+        }
+        setRating(index + 1)
+    }
+
+    // post and patch will both come from submit button you can tell which it is by seeing if there was already an existing rating by checking if the stars are filled or not
+
     return (
         <div css={showStyles}>
-            {error && <ErrorContainer>Error: {error.message}</ErrorContainer>}
+            {error && <ErrorContainer>Error: {error}</ErrorContainer>}
             {loading && <Spinner />}
             {showData && (
                 <div className='showWrapper'>
@@ -163,12 +278,26 @@ function ShowPage() {
                                 <img src={showData.image.medium} alt={`Poster for ${showData.name}`} />
                             )}
                         </div>
-                        <div className='THIS IS WHERE U WANNA PUT THE RATING STARS'>
-                            <h1>Rate this show</h1>
-                            <p>lorem ipsum</p>
+                        <div css={ratingStyles}>
+                            {isRated ?
+                                <h1>You've rated this show</h1> :
+                                <h1>Rate this show</h1>
+                            }
+                            <div className='starContainer'>
+                                {[...Array(5)].map((_, index) => (
+                                    <FontAwesomeIcon
+                                        key={index}
+                                        onClick={() => toggleStar(index)}
+                                        className='star'
+                                        icon={index < rating ? filledStar : emptyStar}
+                                        size='2xl'
+                                    />
+                                ))}
+                            </div>
+                            <button onClick={() => rateShow(showData.id, rating)}>Submit Rating</button>
+                            <button onClick={() => deleteRating(showData.id)}>Delete Rating</button>
                         </div>
                     </div>
-
                     <p className='description' dangerouslySetInnerHTML={{ __html: showData.summary }} />
                 </div>
             )}
