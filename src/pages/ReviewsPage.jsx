17bb67414a13193ea9cc1442a9b82f77b0742c2e
Nico <ngh111201@gmail.com>
2024-10-22 22:35:03 -0700

    update reviews page to display show name and image

--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -1,7 +1,14 @@
 import { css } from '@emotion/react'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as filledStar } from '@fortawesome/free-solid-svg-icons'
+import { faStarHalfStroke as halfStar } from '@fortawesome/free-regular-svg-icons'
+
 import ErrorContainer from '../components/ErrorContainer'
 import Spinner from '../components/Spinner'
+import noImage from "../assets/no-image.jpg"
 
 const PORT = import.meta.env.VITE_PORT
 const userId = 1    // set to 1 until login is implemented
@@ -9,11 +16,42 @@ const userId = 1    // set to 1 until login is implemented
 const reviewsPageStyles = css`
     text-align: center;
 
-    li {
+    ul {
         list-style: none;
+        padding: 0;
+        margin: 2rem;
+        display: grid;
+        gap: 1em;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
+
+    li {
         display: flex;
-        gap: 1rem;
+        flex-direction: column;
+        gap: 0.25rem;
+    }
+
+    li p {
+        text-align: center;
+        color: white;
+        font-size: 1.15rem;
+        margin: 0;
+    }
+
+    li img{
+        border-radius: 10px;
+        box-shadow: 0 4px 8px 0 #222;
+        cursor: pointer;
+    }
+
+    .starContainer {
+        display: flex;
+        gap: 0.25rem;
         justify-content: center;
+    }
+
+    .star {
+        font-size: 1rem;
     }
 `
 
@@ -21,6 +59,7 @@ function ReviewsPage() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const [reviews, setReviews] = useState([])
+    const navigate = useNavigate()
     useEffect(() => {
         async function getReviews() {
             setLoading(true)
@@ -52,14 +91,42 @@ function ReviewsPage() {
                 <ul>
                     {reviews.map(review => (
                         <li key={review.showId}>
-                            <p>
-                                <strong>Show: </strong>
-                                {review.showId}
-                            </p>
-                            <p>
-                                <strong>Rating: </strong>
-                                {review.rating}
-                            </p>
+                            {review.showImage ?
+                                (<img
+                                    src={review.showImage}
+                                    alt={`Poster for ${review.name}`}
+                                    onClick={() => navigate(`/shows/${review.showId}`)} />) :
+                                (
+                                    <img
+                                        className="noImage"
+                                        src={noImage}
+                                        alt="No image available"
+                                        onClick={() => navigate(`/shows/${review.showId}`)}
+                                    />
+                                )}
+                            <br />
+                            <p>{review.showName}</p>
+                            <div className='starContainer'>
+                                {[...Array(5)].map((_, index) => {
+                                    let icon
+                                    if (review.rating >= index + 1) {
+                                        icon = filledStar
+                                    } else if (review.rating > index && rating < index + 1) {
+                                        icon = halfStar
+                                    } else {
+                                        icon = emptyStar
+                                    }
+
+                                    return (
+                                        <FontAwesomeIcon
+                                            key={index}
+                                            className='star'
+                                            icon={icon}
+                                            size='2xl'
+                                        />
+                                    )
+                                })}
+                            </div>
                         </li>
                     ))}
                 </ul>
