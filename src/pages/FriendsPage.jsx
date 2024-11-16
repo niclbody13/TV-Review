dd76fe9447b44bb42459ff6fea6fd103a225201e
niclbody13 <91341366+niclbody13@users.noreply.github.com>
2024-11-15 16:45:43 -0800

    update styles

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,13 +1,55 @@
 import { css } from '@emotion/react'
+import logo from "../assets/television.svg"
 
 const friendsPageStyles = css`
     text-align: center;
+
+    .App-logo {
+        height: 40vmin;
+        pointer-events: none;
+    }
+
+    @media (prefers-reduced-motion: no-preference) {
+        .App-logo {
+        animation: App-logo-spin infinite 10s linear;
+        }
+    }
+
+    .App-header {
+        background-color: #282c34;
+        min-height: 75vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+        color: white;
+    }
+
+    .App-link {
+        color: #61dafb;
+    }
+
+    @keyframes App-logo-spin {
+        from {
+        transform: rotateY(0deg);
+        }
+        to {
+        transform: rotateY(360deg);
+        }
+    }
 `
 
 function FriendsPage() {
     return (
         <div css={friendsPageStyles}>
             <h1>Coming Soon!</h1>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    🚧 Under Development 🚧
+                </p>
+            </header>
         </div>
     )
 }
