4ada595f32cf0cac70ce5f6f0f04ec7b417bc6a3
Nicolas Haralambidis <ngh111201@gmail.com>
2024-12-09 21:22:21 -0800

    migrate from amplify to custom backend

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,17 +1,27 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: true
-    }
-  }
-});
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+//   userAttributes: {
+//     preferredUsername: {
+//       mutable: true,
+//       required: true
+//     }
+//   }
+// });
+
+// import { referenceAuth } from '@aws-amplify/backend';
+
+// export const auth = referenceAuth({
+//   userPoolId: process.env.AWS_USER_POOL_ID!,
+//   identityPoolId: process.env.AWS_IDENTITY_POOL_ID!,
+//   authRoleArn: process.env.AWS_AUTH_ROLE_ARN!,
+//   unauthRoleArn: process.env.AWS_UNAUTH_ROLE_ARN!,
+//   userPoolClientId: process.env.AWS_CLIENT_ID!,
+// });
