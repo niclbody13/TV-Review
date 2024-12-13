15a560b34e041eaadef7a4b50a70f3c77dae50ea
Nicolas Haralambidis <ngh111201@gmail.com>
2024-12-12 22:36:02 -0800

    create initial mobile navbar with basic styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,43 @@
 import { css } from '@emotion/react'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseChimney, faListUl, faUserGroup, faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
 const navbarStyles = css`
     display: flex;
-    justify-content: center;
-    padding: 1rem;
-    margin-top: 1rem;
+    justify-content: space-around;
     font-size: calc(5.5px + 2vmin);
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(13, 13, 13, 0.9);
     
     ul {
         list-style: none;
         padding: 0;
         margin: 0;
         display: flex;
-        gap: 1em;
+        gap: 2em;
     }
 
     li {
         display: inline;
+        display: flex;        
+        justify-content: center;
+        align-items: center;
+    }
+
+    li a {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    li p {
+        padding: 0;
+        margin: 0;
+        font-size: 0.75rem;
     }
 
     a.active {
@@ -33,16 +53,32 @@ const navbarStyles = css`
             background-color: #777;
         }
     }
+
+    .svg-inline--fa {
+        font-size: 1.3rem;
+    }
 `
 
 function Navbar() {
     return (
         <nav css={navbarStyles}>
             <ul>
-                <ActiveLink to="/">Home</ActiveLink>
-                <ActiveLink to="/reviews">My Reviews</ActiveLink>
-                <ActiveLink to="/friends">Friends</ActiveLink>
-                <ActiveLink to="/account">Account</ActiveLink>
+                <ActiveLink to="/">
+                    <FontAwesomeIcon icon={faHouseChimney} />
+                    <p>Home</p>
+                </ActiveLink>
+                <ActiveLink to="/reviews">
+                    <FontAwesomeIcon icon={faListUl} />
+                    <p>Reviews</p>
+                </ActiveLink>
+                <ActiveLink to="/friends">
+                    <FontAwesomeIcon icon={faUserGroup} />
+                    <p>Friends</p>
+                </ActiveLink>
+                <ActiveLink to="/account">
+                    <FontAwesomeIcon icon={faAddressCard} />
+                    <p>Account</p>
+                </ActiveLink>
             </ul>
         </nav>
     )
