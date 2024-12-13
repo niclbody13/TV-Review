1a53bc9adb0f815b01dc47d2aaa508b2658986f5
Nicolas Haralambidis <ngh111201@gmail.com>
2024-12-12 23:09:05 -0800

    add component to load each page from the top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function ScrollToTop() {
-  const location = useLocation();
+  const location = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
+    window.scrollTo(0, 0)
+  }, [location.pathname])
 
-  return null;
+  return null
 }
 
-export default ScrollToTop;
+export default ScrollToTop
