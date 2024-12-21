e38c39bf5de59dd78404b6ff7d4e0112e8bdf328
Nicolas Haralambidis <ngh111201@gmail.com>
2024-12-20 22:15:34 -0800

    add button to delete account

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,38 +1,53 @@
 import { css } from '@emotion/react'
-import { Amplify } from 'aws-amplify'
-import outputs from '../../amplify_outputs.json'
-import { signOut, fetchUserAttributes } from 'aws-amplify/auth'
+import { signOut, fetchUserAttributes, deleteUser } from 'aws-amplify/auth'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-
-Amplify.configure(outputs)
 
 const accountStyles = css`
     text-align: center;    
 
     button {
-        width: 8rem;
+        min-width: 10rem;
         font-size: 1rem;
         color: white;
         background-color: #555;
         box-shadow: 0px 2px 4px 0 #111;
         border-radius: 15px;
-        padding: 0.5rem 0rem;
+        padding: 0.5rem;
         border: 1px solid black;
         cursor: pointer;
+    }
+
+    .buttons {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .deleteAccountModal {
+        border: 2px solid black;
+        padding: 2rem;
+        background-color: rgba(17, 17, 17, 0.75);
+    }
+
+    .deleteAccountModal button {
+        min-width: 4rem;
+        margin: 0 1rem;
     }
 `
 
 function AccountPage() {
     const [userAttributes, setUserAttributes] = useState(null)
+    const [isConfirming, setIsConfirming] = useState(false)
     const navigate = useNavigate()
     useEffect(() => {
         const getUserData = async () => {
             try {
-                const attributes = await fetchUserAttributes();
+                const attributes = await fetchUserAttributes()
                 setUserAttributes(attributes)
             } catch (error) {
-                console.error("Error fetching user attributes:", error);
+                console.error("Error fetching user attributes:", error)
             }
         };
         getUserData();
@@ -43,6 +58,25 @@ function AccountPage() {
         navigate('/')
     }
 
+    const handleDeleteAccount = () => {
+        setIsConfirming(true)
+    }
+
+    const handleConfirmDelete = async () => {
+        try {
+            await deleteUser()
+            alert('Your account has been successfully deleted.')
+            navigate('/')
+        } catch (error) {
+            console.error('Error deleting user:', error)
+            alert('An error occurred while trying to delete your account.')
+        }
+    }
+
+    const handleCancelDelete = () => {
+        setIsConfirming(false)
+    }
+
     return (
         <div css={accountStyles}>
             {userAttributes ? (
@@ -50,7 +84,17 @@ function AccountPage() {
             ) : (
                 <h1>Loading user data...</h1>
             )}
-            <button type='button' onClick={handleSignOut}>Sign out</button>
+            <div className='buttons'>
+                <button type='button' onClick={handleSignOut}>Sign out</button>
+                <button type='button' onClick={handleDeleteAccount}>Delete Account</button>
+                {isConfirming && (
+                    <div className='deleteAccountModal'>
+                        <p>Are you sure? You can't undo this.</p>
+                        <button onClick={handleConfirmDelete} style={{ backgroundColor: '#095109' }}>Yes</button>
+                        <button onClick={handleCancelDelete} style={{ backgroundColor: '#7f0404' }}>No</button>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
